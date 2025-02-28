07b3627f1636ec010af88f4fc6bbecc059a7a9cc
vinay <vinay2752001@gmail.com>
2025-02-28 19:37:35 +0530

    album page and home page

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,14 +1,45 @@
-import './App.css';
-import Hero from './components/Hero/Hero';
-import Navbar from './components/Navbar/Navbar';
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import StyledEngineProvider from "@mui/material/StyledEngineProvider";
+import { Outlet } from "react-router-dom";
+import {
+  fetchFilters,
+  fetchNewAlbums,
+  fetchSongs,
+  fetchTopAlbums,
+} from "./api/api";
 
 function App() {
+  const [data, setData] = useState({});
+
+
+  const generateData = (key, source) => {
+    source().then((data) => {
+      setData((prevState) => {
+        // Object.assign would also work
+        return { ...prevState, [key]: data };
+      });
+    });
+  };
+
+  useEffect(() => {
+    generateData("topAlbums", fetchTopAlbums);
+    generateData("newAlbums", fetchNewAlbums);
+    generateData("songs", fetchSongs);
+    generateData("genres", fetchFilters);
+  }, []);
+
+  const { topAlbums = [], newAlbums = [], songs = [], genres = [] } = data;
+
   return (
-    <div className="App">
-        <Navbar/>
-        <Hero/>
-    </div>
+    <>
+      <StyledEngineProvider injectFirst>
+        <Navbar searchData={[...topAlbums, ...newAlbums]} />
+        <Outlet context={{ data: { topAlbums, newAlbums, songs, genres } }} />
+      </StyledEngineProvider>
+    </>
   );
 }
+
 
 export default App;
